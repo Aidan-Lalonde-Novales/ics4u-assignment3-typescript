d5b5dc286440145889dffe93f06b428e262c20c1
aaa <71395339+Aidan-Lalonde-Novales@users.noreply.github.com>
2022-11-01 19:16:18 -0400

    ight

--- src/index.ts
+++ src/index.ts
@@ -1,58 +1,94 @@
 /**
  * This is the main code that runs
- * with the Vehicle Class.
+ * with the Triangle Class.
  *
  * By:      Aidan Lalonde-Novales (mostly Mr Coxall)
  * Version: 1.0
  * Since:   2022-11-01
  */
 
-import Bike from './Bike'
-import Truck from './Truck'
+ import Triangle from './Triangle'
 
-// new BMX Bike
-console.log('Created BMX Bike.')
-const bmxBike = new Bike(40, 'Red')
-
-console.log('Bike Status:')
-bmxBike.status()
-
-console.log('Set the cadence to 10:')
-bmxBike.setCadence(10)
-bmxBike.accelerate(0)
-bmxBike.status()
-
-console.log('Accelerate by 15:')
-bmxBike.accelerate(15)
-bmxBike.status()
-
-console.log('Braking, 5 of power for 2 seconds:')
-bmxBike.braking(5, 2)
-bmxBike.status()
-
-console.log('Ring Bell:')
-bmxBike.ringBell()
-
-console.log('')
-
-// new Truck
-console.log('Created Truck.')
-const truck = new Truck(200, 'Grey')
-truck.setLicensePlate('HGC-3456F')
-
-console.log('Truck Status:')
-truck.status()
-
-console.log('Accelerating, 10 of power for 10 seconds:')
-truck.accelerate(10, 10)
-console.log(`\nNew speed (1): ${Number(truck.getSpeed())}\n`)
-
-console.log('Set tire Air Pressure to 5:')
-truck.setAirPressure(5)
-console.log(`\nNew Air Pressure: ${Number(truck.getAirPressure())}\n`)
-
-console.log('Braking, 10 of power for 2 seconds:')
-truck.braking(10, 2)
-console.log(`\nNew speed (2): ${Number(truck.getSpeed())}\n`)
-
-console.log('Done.')
+ console.log('')
+ 
+ const testTriangle1 = new Triangle(3, 4, 5)
+ console.log('Created a 3 mm by 4 mm by 5 mm triangle.')
+ console.log(' → Is the triangle valid: ' + testTriangle1.isValid().toString())
+ console.log(' → Semiperimeter: ' + testTriangle1.semiPerimeter().toFixed(4) + ' mm')
+ console.log(' → Area: ' + testTriangle1.area().toFixed(4) + ' mm²')
+ console.log(' → Type: ' + testTriangle1.getType())
+ console.log(' → Angle 1: ' + testTriangle1.angle(1).toFixed(4) + ' rad')
+ console.log(' → Angle 2: ' + testTriangle1.angle(2).toFixed(4) + ' rad')
+ console.log(' → Angle 3: ' + testTriangle1.angle(3).toFixed(4) + ' rad')
+ console.log(' → Height 1: ' + testTriangle1.height(1).toFixed(4) + ' mm')
+ console.log(' → Height 2: ' + testTriangle1.height(2).toFixed(4) + ' mm')
+ console.log(' → Height 3: ' + testTriangle1.height(3).toFixed(4) + ' mm')
+ console.log(' → Inner circle radius: ' + testTriangle1.innerCircleRadius().toFixed(4) + ' mm')
+ console.log(' → Circumcircle radius: ' + testTriangle1.circumsicleRadius().toFixed(4) + ' mm')
+ console.log('')
+ 
+ const testTriangle2 = new Triangle(3, 3, 3)
+ console.log('Created a 3 mm by 3 mm by 3 mm triangle.')
+ console.log(' → Is the triangle valid: ' + testTriangle2.isValid().toString())
+ console.log(' → Semiperimeter: ' + testTriangle2.semiPerimeter().toFixed(4) + ' mm')
+ console.log(' → Area: ' + testTriangle2.area().toFixed(4) + ' mm²')
+ console.log(' → Type: ' + testTriangle2.getType())
+ console.log(' → Angle 1: ' + testTriangle2.angle(1).toFixed(4) + ' rad')
+ console.log(' → Angle 2: ' + testTriangle2.angle(2).toFixed(4) + ' rad')
+ console.log(' → Angle 3: ' + testTriangle2.angle(3).toFixed(4) + ' rad')
+ console.log(' → Height 1: ' + testTriangle2.height(1).toFixed(4) + ' mm')
+ console.log(' → Height 2: ' + testTriangle2.height(2).toFixed(4) + ' mm')
+ console.log(' → Height 3: ' + testTriangle2.height(3).toFixed(4) + ' mm')
+ console.log(' → Inner circle radius: ' + testTriangle2.innerCircleRadius().toFixed(4) + ' mm')
+ console.log(' → Circumcircle radius: ' + testTriangle2.circumsicleRadius().toFixed(4) + ' mm')
+ console.log('')
+ 
+ const testTriangle3 = new Triangle(3, 4, 4)
+ console.log('Created a 3 mm by 4 mm by 4 mm triangle.')
+ console.log(' → Is the triangle valid: ' + testTriangle3.isValid().toString())
+ console.log(' → Semiperimeter: ' + testTriangle3.semiPerimeter().toFixed(4) + ' mm')
+ console.log(' → Area: ' + testTriangle3.area().toFixed(4) + ' mm²')
+ console.log(' → Type: ' + testTriangle3.getType())
+ console.log(' → Angle 1: ' + testTriangle3.angle(1).toFixed(4) + ' rad')
+ console.log(' → Angle 2: ' + testTriangle3.angle(2).toFixed(4) + ' rad')
+ console.log(' → Angle 3: ' + testTriangle3.angle(3).toFixed(4) + ' rad')
+ console.log(' → Height 1: ' + testTriangle3.height(1).toFixed(4) + ' mm')
+ console.log(' → Height 2: ' + testTriangle3.height(2).toFixed(4) + ' mm')
+ console.log(' → Height 3: ' + testTriangle3.height(3).toFixed(4) + ' mm')
+ console.log(' → Inner circle radius: ' + testTriangle3.innerCircleRadius().toFixed(4) + ' mm')
+ console.log(' → Circumcircle radius: ' + testTriangle3.circumsicleRadius().toFixed(4) + ' mm')
+ console.log('')
+ 
+ const testTriangle4 = new Triangle(2, 3, 4)
+ console.log('Created a 2 mm by 3 mm by 4 mm triangle.')
+ console.log(' → Is the triangle valid: ' + testTriangle4.isValid().toString())
+ console.log(' → Semiperimeter: ' + testTriangle4.semiPerimeter().toFixed(4) + ' mm')
+ console.log(' → Area: ' + testTriangle4.area().toFixed(4) + ' mm²')
+ console.log(' → Type: ' + testTriangle4.getType())
+ console.log(' → Angle 1: ' + testTriangle4.angle(1).toFixed(4) + ' rad')
+ console.log(' → Angle 2: ' + testTriangle4.angle(2).toFixed(4) + ' rad')
+ console.log(' → Angle 3: ' + testTriangle4.angle(3).toFixed(4) + ' rad')
+ console.log(' → Height 1: ' + testTriangle4.height(1).toFixed(4) + ' mm')
+ console.log(' → Height 2: ' + testTriangle4.height(2).toFixed(4) + ' mm')
+ console.log(' → Height 3: ' + testTriangle4.height(3).toFixed(4) + ' mm')
+ console.log(' → Inner circle radius: ' + testTriangle4.innerCircleRadius().toFixed(4) + ' mm')
+ console.log(' → Circumcircle radius: ' + testTriangle4.circumsicleRadius().toFixed(4) + ' mm')
+ console.log('')
+ 
+ const testTriangle5 = new Triangle(23, 4, 6)
+ console.log('Created a 23 mm by 3 mm by 4 mm triangle.')
+ console.log(' → Is the triangle valid: ' + testTriangle5.isValid().toString())
+ console.log(' → Semiperimeter: ' + testTriangle5.semiPerimeter().toFixed(4) + ' mm')
+ console.log(' → Area: ' + testTriangle5.area().toFixed(4) + ' mm²')
+ console.log(' → Type: ' + testTriangle5.getType())
+ console.log(' → Angle 1: ' + testTriangle5.angle(1).toFixed(4) + ' rad')
+ console.log(' → Angle 2: ' + testTriangle5.angle(2).toFixed(4) + ' rad')
+ console.log(' → Angle 3: ' + testTriangle5.angle(3).toFixed(4) + ' rad')
+ console.log(' → Height 1: ' + testTriangle5.height(1).toFixed(4) + ' mm')
+ console.log(' → Height 2: ' + testTriangle5.height(2).toFixed(4) + ' mm')
+ console.log(' → Height 3: ' + testTriangle5.height(3).toFixed(4) + ' mm')
+ console.log(' → Inner circle radius: ' + testTriangle5.innerCircleRadius().toFixed(4) + ' mm')
+ console.log(' → Circumcircle radius: ' + testTriangle5.circumsicleRadius().toFixed(4) + ' mm')
+ console.log('')
+ 
+ console.log('\nDone.') 
